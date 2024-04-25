bf8ff485d77b65662cc8e372f2e3bdf18ffb6647
83343 <aseelabdulghafour.129@gmail.com>
2024-04-25 11:42:00 +0300

    Hi

---
+++ Basic/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Aseel";
+int age = 24;
+double phone = 30.30;
+bool status = false;
+
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine(phone);
+Console.WriteLine(status);
+
+double result = Convert.ToDouble(age);
+Console.WriteLine(result);
+/////task2
+var student_name = "Aseel Abdulghafour";
+char student_Grade = 'A';
+var fav_subject = "History";
+
+Console.WriteLine("my name is " + student_name);
+Console.WriteLine("My Grade Is " + student_Grade);
+Console.WriteLine("My Favorite Subject is " + fav_subject);
+
+var height = 8;
+var width = 6;
+Console.WriteLine("the area of the rectangle is " + height * width);
